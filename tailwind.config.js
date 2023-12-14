bcf3ffabccb5f2b066e817aa5fe9829ca4a6988e
adithyachandranv <adithyachandranv@gmail.com>
2023-12-14 22:39:10 +0530

    started tailwind development

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
